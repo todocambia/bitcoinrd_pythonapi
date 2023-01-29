cc2cd3e895ad1f94d652b715100e916053ed9ddd
todocambia <123221144+todocambia@users.noreply.github.com>
2023-01-29 10:26:35 -0400

    trades function added

--- client.py
+++ client.py
@@ -41,11 +41,11 @@ def get_trades():
     return data
 
 
-api_key = "YOU_API_KEY"
-api_secret = "YOUR_API_SECRET"
+api_key = "API_USER_KEY"
+api_secret = "API_USER_SECRET"
 
 
-# initialize the following variables: method, path and api_expires.
+# initialize the following variables: method, patßh and api_expires.
 def get_api_expires():
     return str(int(time.time() + 60))
 
@@ -70,6 +70,8 @@ def generate_signature(method, path, api_expires):
     signature = hmac.new(api_secret.encode(),string_to_encode.encode(),hashlib.sha256).hexdigest()
     return signature
 
+#User balance function
+
 def get_balance(api_key, api_secret):
     method, path, api_expires = init_signature()
     headers = auth_me(api_key, api_secret, method, path)
@@ -82,5 +84,27 @@ def get_balance(api_key, api_secret):
         return "Error: " + str(response.status_code)
 
 
-result = get_balance(api_key, api_secret)
-print(result)
+
+#User trades function
+
+def get_trades(api_key, api_secret):
+    method = "GET"
+    path = "/v2/user/trades"
+    api_expires = get_api_expires()
+    signature = generate_signature(method, path, api_expires)
+    headers = {
+        "api-key": api_key,
+        "api-signature": signature,
+        "api-expires": api_expires
+    }
+    response = requests.get("https://api.bitcoinrd.do" + path, headers=headers)
+
+    if response.status_code == 200:
+        print(response.json())
+        return response.json()
+    else:
+        print("Error: " + str(response.status_code))
+        return "Error: " + str(response.status_code)
+
+    
+
